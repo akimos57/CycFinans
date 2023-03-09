3de6204141d3bad8260fff8090c2d101f19b9e3f
akimos57 <akim_tokarev@mail.ru>
2023-03-09 14:33:10 +0300

    small update

--- app/src/main/java/ru/cyclone/cycfinans/presentation/screens/statistics/StatisticsScreen.kt
+++ app/src/main/java/ru/cyclone/cycfinans/presentation/screens/statistics/StatisticsScreen.kt
@@ -104,7 +104,23 @@ fun StatisticsScreen(navController: NavHostController) {
                 currentYear = currentYear,
                 confirmButtonClicked = {
                         month_, year_ ->
-                    date = "$month_, $year_"
+//                    date = "$month_, $year_"
+                    val i = when(month_){
+                        1 -> "Январь"
+                        2 -> "Февраль"
+                        3 -> "Март"
+                        4 -> "Апрель"
+                        5 -> "Май"
+                        6 -> "Июнь"
+                        7 -> "Июль"
+                        8 -> "Август"
+                        9 -> "Сентябрь"
+                        10 -> "Октябрь"
+                        11 -> "Ноябрь"
+                        12 -> "Декабрь"
+                        else -> {}
+                    }
+                    date = "$i, $year_"
                     visible = false
                 },
                 cancelClicked = {
@@ -112,15 +128,6 @@ fun StatisticsScreen(navController: NavHostController) {
                 },
                 onDismiss = { visible = false }
             )
-            Row(
-                modifier = Modifier
-            ) {
-                Text(
-                    text = date,
-                    modifier = Modifier
-                        .clickable { visible = true }
-                )
-            }
 
         }
         
